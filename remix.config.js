54e6462910efd9ccd896078c67794427d9e38f29
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-07-22 14:59:24 +0200

    Deploy to Netlify Edge Functions

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",
+  serverBuildTarget: "netlify-edge",
   server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? "./server.js" : undefined,
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // publicPath: "/build/",
+  ignoredRouteFiles: [".*"],
 }
